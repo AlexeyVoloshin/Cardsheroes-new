c739bf285be0dd583aa080ddb2e36104462c4ecd
AlexeyVoloshin <voloshinalex1100@gmail.com>
2019-08-29 13:22:06 +0300

    Update message.blade.php

--- resources/views/editor/part/message.blade.php
+++ resources/views/editor/part/message.blade.php
@@ -1,30 +1,48 @@
-    {{--@if(Session::has('success'))--}}
-        {{--<div class="alert alert-success">--}}
-            {{--{{ Session::get('success') }}--}}
-        {{--</div>--}}
+<script>
+    function Close_button(text, aler) {
+
+        var team_div = document.getElementById('team_div');
+        var container = document.createElement("div");
+        container.className = 'hide alert '+aler+'';
+        container.role = 'alert';
+        container.id= 'hide';
+        container.innerHTML = '<button type="button" class="close" id="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true" >x</span>' +
+            '</button>'+
+            ''+text+'';
+        team_div.appendChild(container);
+    }
+</script>
     @if(session('success'))
         <div class="row justify-content-center">
-            <div class="col-md-11">
-                <div class="alert alert-success" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">x</span>
-                    </button>
-                    {{Session::get('success')}}
-                </div>
+            <div class="col-md-11 " id="team_div">
+                    <script>
+                        var messAler = 'alert-success';
+                        var mess = {!! json_encode(Session::get('success')) !!}
+                        Close_button(mess, messAler);
+                    </script>
             </div>
         </div>
     @endif
-    {{--@if(Session::has('errors'))--}}
-    @if($errors->any())
-        <div class="row justify-content-center">
-            <div class="col-md-11">
-                <div class="alert alert-danger" role="alert">
 
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">x</span>
-                    </button>
-                    {{  $errors->first() }}
-                </div>
+    @if($errors->any())
+
+        <div class="row justify-content-center">
+            <div class="col-md-11" id="team_div">
+                  <script>
+                       var messAler = 'alert-danger';
+                       var mess = {!! json_encode($errors->first()) !!}
+                       Close_button(mess, messAler);
+                  </script>
             </div>
         </div>
     @endif
+
+<script>
+    document.getElementById("hide").onclick =  function () {
+        console.log('hi');
+        var test =  document.getElementsByClassName('hide')[0];
+        if(test){
+            test.parentNode.removeChild(test);
+        }
+    }
+</script>
